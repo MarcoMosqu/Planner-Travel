61f1d8011513ead7981e4978e58b9aed2d2545bf
MarcoMosqu <139501449+MarcoMosqu@users.noreply.github.com>
2025-12-12 00:08:38 -0700

    V1.1 Main.java

--- Main.java
+++ Main.java
@@ -1,24 +1,34 @@
 //////////////////////////////////////
 // Class : Main
 //////////////////////////////////////
+import javafx.application.Application;
+import javafx.stage.Stage;
 import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) {
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
         Scanner in = new Scanner(System.in);
 
         UserProfile user = new UserProfile();
         user.collectUserInfo(in);
-
+        // Uses JavaFX dialogs inside determineCountry()
         CountryAdvisor advisor = new CountryAdvisor();
-        advisor.determineCountry(in, user);
+        advisor.determineCountry(user);  
 
         TravelDisplay display = new TravelDisplay();
         display.showPrologue(user, advisor, in);
 
-        //advisor.suggestWeeklyPlan(in, user);
-
         in.close();
+
+        // Close the JavaFX window when done 
+        primaryStage.close();
+    }
+
+    public static void main(String[] args) {
+        launch(args);  // Required to init JavaFX Toolkit
     }
 }
+
 
